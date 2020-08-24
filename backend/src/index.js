ed4ada583060464c510c9f92ffb5f8629fdbfe64
lufial-dev <lufial.dev@gmail.com>
2020-08-23 21:19:04 -0300

    Aula 22. Cadastrando uma nova Tarefa

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const server = express();
+const TaskRouter = require('./router/TaskRouter');
+
+server.use(express.json());
+server.use("/task", TaskRouter);
+
 
 server.get('/teste', (req, res)=>{
     res.send("Helo Word! Bem vindo ao todo ")
