64292295d4d0c9313a6b14cebe60a35cfdf37613
lufial-dev <lufial.dev@gmail.com>
2020-08-29 17:23:33 -0300

    41. Criando o Footer com Styled Components

--- web/src/views/Home/index.js
+++ web/src/views/Home/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import * as S from './style';
 
 function Home() {
   return (
     <S.Container>
       <Header />
+
+      <Footer />
     </S.Container>
       
   );
---
+++ web/src/views/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './style';
+
+function Footer(){
+    return(
+        <S.Container>
+            <span>ToDo - Organizando a sua vida</span>
+        </S.Container>
+    );
+
+}
+
+export default Footer;
